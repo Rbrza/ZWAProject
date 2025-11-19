fb86c6ded065017bcd159adb2a4a6eaf723e2a41
Rbrza <r.brza1@gmail.com>
2025-11-19 13:01:32 +0100

    Added sign up page

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Happy developing ✨')
+function checkPassword(event) {
+    event.preventDefault();
+
+    const form = document.getElementById('form');
+    const password = document.getElementById("password").value;
+    const password2 = document.getElementById("password2").value;
+    const password2Input = document.getElementById("password2");
+
+    if (password === password2) {
+        form.submit()
+    } else if (password !== password2) {
+        alert("Hesla se neshodují");
+        const password2Wrapper = document.getElementById("password2-wrap");
+        password2Wrapper.classList.add("error");
+        password2Input.classList.add("error");
+    }
+}
+
+function resetStyles() {
+    const password2 = document.getElementById("password2");
+    password2.classList.remove("error")
+}
+
+document.getElementById("form").addEventListener("submit", checkPassword);
+
+document.getElementById("password2").addEventListener("focus", resetStyles)
